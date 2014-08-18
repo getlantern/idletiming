47fbdd42abd4a5a148cc3f1b38551cda546d5039
Ox Cart <ox.to.a.cart@gmail.com>
2014-08-18 16:19:02 -0500

    Added note about timeout being up to twice specified value

--- idletiming_conn.go
+++ idletiming_conn.go
@@ -11,7 +11,8 @@ import (
 // the specified period.
 //
 // idleTimeout specifies how long to wait for inactivity before considering
-// connection idle.
+// connection idle.  Note - the actual timeout may be up to twice idleTimeout,
+// depending on timing.
 //
 // onIdle is an optional function to call after the connection has been closed
 func Conn(conn net.Conn, idleTimeout time.Duration, onIdle func()) net.Conn {
--- idletiming_listener.go
+++ idletiming_listener.go
@@ -10,7 +10,8 @@ import (
 // specified duration.
 //
 // idleTimeout specifies how long to wait for inactivity before considering
-// connection idle.
+// connection idle.  Note - the actual timeout may be up to twice idleTimeout,
+// depending on timing.
 //
 // onIdle is an optional function to call after the connection has been closed
 func Listener(listener net.Listener, idleTimeout time.Duration, onIdle func()) net.Listener {
